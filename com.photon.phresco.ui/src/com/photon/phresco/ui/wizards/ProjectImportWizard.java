201a448c504b27302adc43d50031d885a49c0d35
Arunachalam Lakshmanan <arunachalam.lakshmanan@photoninfotech.net>
2012-06-27 19:35:52 +0530

    Added import wizards and externalized strings in plugin.xml & manifest.mf

---
+++ com.photon.phresco.ui/src/com/photon/phresco/ui/wizards/ProjectImportWizard.java
@@ -0,0 +1,49 @@
+/*
+ * ###
+ * 
+ * Copyright (C) 1999 - 2012 Photon Infotech Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ###
+ */
+package com.photon.phresco.ui.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+/**
+ * Import existing project from file system
+ * @author arunachalam.lakshmanan@photoninfotech.net
+ *
+ */
+public class ProjectImportWizard extends Wizard implements IImportWizard {
+
+	public ProjectImportWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
