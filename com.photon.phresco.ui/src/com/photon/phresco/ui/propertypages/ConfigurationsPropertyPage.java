5b8cc56053d0b12efd5dde0a8d29b20f586b53eb
Arunachalam Lakshmanan <arunachalam.lakshmanan@photoninfotech.net>
2012-06-27 23:42:44 +0530

    Added baseline code for project configurations page

---
+++ com.photon.phresco.ui/src/com/photon/phresco/ui/propertypages/ConfigurationsPropertyPage.java
@@ -0,0 +1,21 @@
+package com.photon.phresco.ui.propertypages;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class ConfigurationsPropertyPage extends PropertyPage implements
+		IWorkbenchPropertyPage {
+
+	public ConfigurationsPropertyPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
