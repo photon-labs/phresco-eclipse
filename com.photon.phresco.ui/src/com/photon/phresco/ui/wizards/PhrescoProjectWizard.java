dfb5539058c3718757b09eff7584d91e07eca5d3
hariharan <hariharan.t@photoninfotech.net>
2012-08-31 15:21:07 +0530

    artf525139: Build Related Issue Fixes and Android Feature Enhancement Reviewer : Arun

--- com.photon.phresco.ui/src/com/photon/phresco/ui/wizards/PhrescoProjectWizard.java
+++ com.photon.phresco.ui/src/com/photon/phresco/ui/wizards/PhrescoProjectWizard.java
@@ -20,6 +20,7 @@ package com.photon.phresco.ui.wizards;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
