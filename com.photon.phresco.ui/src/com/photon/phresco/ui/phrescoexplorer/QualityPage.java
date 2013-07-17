ba5d8a55f04925356de58976c12b70cb9c13689e
saravanan-na <saravanan.natarajan@photoninfotech.net>
2013-07-17 18:38:34 +0530

    QualityPage.java added

---
+++ com.photon.phresco.ui/src/com/photon/phresco/ui/phrescoexplorer/QualityPage.java
@@ -0,0 +1,16 @@
+package com.photon.phresco.ui.phrescoexplorer;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class QualityPage extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+}
