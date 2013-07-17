cbf5f5165bd453ec19d7d4a5c3f2fbd916d6431c
suresh-ma <suresh.marimuthu@photoninfotech.net>
2013-07-17 11:23:28 +0530

    phrescoutil getserviceManager method added

--- com.photon.phresco.ui/src/com/photon/phresco/commons/util/PhrescoUtil.java
+++ com.photon.phresco.ui/src/com/photon/phresco/commons/util/PhrescoUtil.java
@@ -1,41 +1,44 @@
 package com.photon.phresco.commons.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.photon.phresco.commons.PhrescoConstants;
-import com.photon.phresco.commons.model.User;
 import com.photon.phresco.exception.PhrescoWebServiceException;
 import com.photon.phresco.service.client.api.ServiceContext;
 import com.photon.phresco.service.client.api.ServiceManager;
-import com.photon.phresco.service.client.factory.ServiceClientFactory;
+import com.photon.phresco.service.client.impl.ServiceManagerImpl;
 
 
 public class PhrescoUtil implements PhrescoConstants {
 
 	static boolean isLoggedIn = false;
+	public static final Map<String, ServiceManager> CONTEXT_MANAGER_MAP = new HashMap<String, ServiceManager>();
 	
 	public static boolean doLogin(String userName, String password) throws PhrescoWebServiceException {
 		
-		if (!isLoggedIn) {
-			ServiceContext context = new ServiceContext();
-	        context.put(SERVICE_URL, "http://172.16.8.250:7070/service-testing/rest/api");
-			System.out.println(" user name in phresco util : " + userName);
-			System.out.println(" password in phresco util : " + password);
-	        context.put(SERVICE_USERNAME, userName);
-	        context.put(SERVICE_PASSWORD, password);
-	        System.out.println("ServiceUrl--------> " + context.get(SERVICE_URL));
-	        try {
-	        	ServiceManager serviceManager = ServiceClientFactory.getServiceManager(context);
-	        	User userInfo = serviceManager.getUserInfo();
-	        	System.out.println(" user name : " + userInfo.getDisplayName());
-	        	isLoggedIn = true;
-	        } catch(Exception e) {
-	        	e.printStackTrace();
-	        }
-		}
-		
+		ServiceContext context = new ServiceContext();
+        context.put(SERVICE_URL, "http://localhost:3030/service/rest/api");
+		System.out.println(" user name in phresco util : " + userName);
+		System.out.println(" password in phresco util : " + password);
+        context.put(SERVICE_USERNAME, userName);
+        context.put(SERVICE_PASSWORD, password);
+        try {
+        	ServiceManager serviceManager = new ServiceManagerImpl(context);
+        	CONTEXT_MANAGER_MAP.put(userName, serviceManager);
+        	isLoggedIn = true;
+        } catch(Exception e) {
+        	isLoggedIn = false;
+        	e.printStackTrace();
+        }
         return isLoggedIn;
 	}
 	
 	public static boolean isLoggedIn() {
 		return isLoggedIn;
+	}
+	
+	public static ServiceManager getServiceManager(String userId) {
+		return CONTEXT_MANAGER_MAP.get(userId);
 	}
 }
