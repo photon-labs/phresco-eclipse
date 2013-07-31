9c6f03581735424a2c57fe572d478f507bd8951d
Syed <syed.ahamed@photoninfotech.net>
2013-07-31 16:09:28 +0530

    integrate with service and showing in ui done

--- com.photon.phresco.ui/src/com/photon/phresco/ui/phrescoexplorer/wizard/JSLibraryFeaturePage.java
+++ com.photon.phresco.ui/src/com/photon/phresco/ui/phrescoexplorer/wizard/JSLibraryFeaturePage.java
@@ -19,7 +19,9 @@
 
 package com.photon.phresco.ui.phrescoexplorer.wizard;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
@@ -27,7 +29,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 
 import com.photon.phresco.commons.model.ArtifactGroup;
 
@@ -67,17 +68,6 @@ public class JSLibraryFeaturePage extends AbstractFeatureWizardPage {
 		
 	}
 	
-	public void getSelectedItems() {
-		if (jsLibTable != null) {
-			TableItem[] selection = jsLibTable.getSelection();
-			for (int i = 0; i < selection.length; i++) {
-				TableItem tableItem = selection[i];
-				String id = (String) tableItem.getData();
-				System.out.println(" ID IN JS LIB : " + id);
-			}
-		}
-	}
-
 	@Override
 	public void renderPage() {
 		
@@ -90,11 +80,29 @@ public class JSLibraryFeaturePage extends AbstractFeatureWizardPage {
 		final ScrolledComposite scrolledComposite = new ScrolledComposite(parentComposite, SWT.V_SCROLL);
 		scrolledComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
 		scrolledComposite.setAlwaysShowScrollBars(false);
-		
-		scrolledComposite.setLocation(5, 5);
-		scrolledComposite.setBounds(5, 5, 500, 200);
+		scrolledComposite.setBounds(5, 5, 500, 350);
 		
 		jsLibTable = getFeatureTable(scrolledComposite, PAGE_NAME, features);
 	}
 	
+	public void getSelectedItems() {
+		System.out.println(" getSelectedCheckBoxRows : " + getSelectedCheckBoxRows());
+		Map<ArtifactGroup, String> selectedComboBoxRows = getSelectedComboBoxRows();
+		Map<ArtifactGroup, String> selectedCheckBoxRows = getSelectedCheckBoxRows();
+		
+		Iterator entries = selectedCheckBoxRows.entrySet().iterator();
+		while (entries.hasNext()) {
+		    Map.Entry entry = (Map.Entry) entries.next();
+		    ArtifactGroup key = (ArtifactGroup)entry.getKey();
+		    
+		    String value = (String)entry.getValue();
+		    System.out.println("Key = " + key.getDisplayName() + " = " + value);
+		    
+		    if (value != null && KEY_EMPTY.equals(value)) {
+		    	String comboValue = selectedComboBoxRows.get(key);
+		    	System.out.println(" comboValue :" + comboValue);
+		    }
+		}
+		
+	}
 }
